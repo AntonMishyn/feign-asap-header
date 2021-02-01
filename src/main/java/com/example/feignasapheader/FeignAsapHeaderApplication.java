27624c77b545b43f3ab5e1cbedcb8dac13ce6806
Anton Mishyn <mishinaa333@gmail.com>
2021-02-01 16:52:24 +0200

    added untracked files

--- src/main/java/com/example/feignasapheader/FeignAsapHeaderApplication.java
+++ src/main/java/com/example/feignasapheader/FeignAsapHeaderApplication.java
@@ -1,15 +1,20 @@
 package com.example.feignasapheader;
 
 import com.example.feignasapheader.client.HelloFeign;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.net.ConnectException;
+
 @SpringBootApplication
 @EnableFeignClients
 @RequiredArgsConstructor
+@Slf4j
 public class FeignAsapHeaderApplication implements CommandLineRunner {
 
     private final HelloFeign helloFeign;
@@ -19,8 +24,12 @@ public class FeignAsapHeaderApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) {
-        String response = helloFeign.getHello();
-        System.out.println(response);
+    public void run(String... args) throws Exception {
+        String responseBody = null;
+        try {
+            responseBody = helloFeign.getHello();
+        } catch (FeignException | ConnectException e) {
+        }
+        log.info(responseBody);
     }
 }
--- src/main/java/com/example/feignasapheader/client/HelloFeign.java
+++ src/main/java/com/example/feignasapheader/client/HelloFeign.java
@@ -4,9 +4,11 @@ import com.example.feignasapheader.config.HelloClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.net.ConnectException;
+
 @FeignClient(name = "helloClient", configuration = HelloClientConfig.class, url = "${asap.client.hello.resource.server.url}")
 public interface HelloFeign {
 
     @GetMapping
-    String getHello();
+    String getHello() throws ConnectException;
 }
